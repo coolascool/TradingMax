2dd04118b6f9572b65f1ca24d4f54ef59f9d192e
coolascool <aniket.787068@gmail.com>
2024-02-18 15:38:34 +0530

    navbar

---
+++ src/js/script.js
@@ -0,0 +1,70 @@
+// navbar started ----->
+
+// small screen
+var card = document.getElementById("card");
+var maincontainer = document.getElementById("main-container");
+function openmenu() {
+  card.style.right = "0";
+}
+function closemenu() {
+  card.style.right = "-275px";
+}
+
+// sign in
+function togglePopupsignin() {
+  let popup = document.getElementById("signin-popup");
+  let overlay = document.querySelector(".overlaysignin");
+
+  if (popup.style.display === "block") {
+    popup.style.display = "none";
+    overlay.style.display = "none";
+  } else {
+    popup.style.display = "block";
+    overlay.style.display = "block";
+  }
+}
+function signIn() {
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  togglePopup();
+}
+
+// sign up
+function togglePopupsignup() {
+  let popup = document.getElementById("signup-popup");
+  let overlay = document.querySelector(".overlaysignup");
+
+  if (popup.style.display === "block") {
+    popup.style.display = "none";
+    overlay.style.display = "none";
+  } else {
+    popup.style.display = "block";
+    overlay.style.display = "block";
+  }
+}
+function signUp() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let mobile = document.getElementById("mobile").value;
+  let password = document.getElementById("password").value;
+  togglePopup();
+  return false;
+}
+
+//switch signin-signup
+function newuser(){
+  let popup = document.getElementById("signin-popup");
+  let overlay = document.querySelector(".overlaysignin");
+  popup.style.display='none';
+  overlay.style.display='none';
+  togglePopupsignup();
+}
+function olduser(){
+  let popup = document.getElementById("signup-popup");
+  let overlay = document.querySelector(".overlaysignup");
+  popup.style.display='none';
+  overlay.style.display='none';
+  togglePopupsignin();
+}
+
+// -----> navbar ended
